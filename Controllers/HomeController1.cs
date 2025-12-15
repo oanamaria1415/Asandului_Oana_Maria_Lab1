c971da78bbed055bcbf12e127e6821d9d4a27bf9
Oana-Maria Asandului <oanamaria1415@yahoo.com>
2025-12-15 18:21:51 +0200

    controller creat

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asandului_Oana_Maria_Lab1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+
+        public string Index(string nume, int varsta)
+        {
+
+            return "Salut " + nume + ", ai varsta de " + varsta + " ani.";
+        }
+    }
+}
